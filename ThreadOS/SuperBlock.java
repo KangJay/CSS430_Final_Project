d5b942ecd64393aa3bd51df07a11c06a0a6abe5f
Ji <jkang18@uw.edu>
2019-03-07 12:44:16 -0800

    SuperBlock.java under construction

---
+++ ThreadOS/SuperBlock.java
@@ -0,0 +1,65 @@
+/**
+
+*/
+
+public class Superblock
+{
+    private final int defaultInodeBlocks = 64;
+    //Locations within the super block that shows the details of the disk
+    private final int totalBlocksLoc = 0;
+    private final int totalInodeLoc = 4;
+    private final int freeListLoc = 8;
+
+    public int totalBlocks;
+    public int totalInods;
+    public int freeList;
+
+    // Disk.blockSize = 512. Static variable
+    public SuperBlock( int diskSize )
+    {
+        byte[] superBlock = new byte[Disk.blockSize];
+        SysLib.rawread(0, superBlock); // Reads the first block from Disk into the superblock
+
+        //Get the total from the offset locations within the superblock
+        totalBlocks = SysLib.bytes2int(superblock, totalBlocksLoc);
+        totalInodes = SysLib.bytes2int(superBlock, totalInodeLoc);
+        freeList = SysLib.bytes2int(superBlock, freeListLoc);
+
+        if (totalBlocks == diskSize && totalInodes > 0 && freeList >= 2)
+        {
+            //The disk's contents are valid
+            return;
+        }
+        else
+        {
+            // Need to format/re-format the disk
+            totalBlocks = diskSize;
+            format(defaultInodeBlocks); //Implement format
+        }
+    }
+
+    public void format(int diskSize)
+    {
+
+    }
+
+    public void sync()
+    {
+        /* In order to sync the contents of the superblock to disk, it is necessary to convert all
+        necessary data to byte format within a temporary byte array, which then gets written to disk*/
+        
+        byte[] tempSuperBlock = new byte[Disk.blockSize] //should be 512 (required size for rawread/rawwrite)
+        /*
+        totalBlocks = SysLib.bytes2int(superblock, totalBlocksLoc);
+        totalInodes = SysLib.bytes2int(superBlock, totalInodeLoc);
+        freeList = SysLib.bytes2int(superBlock, freeListLoc);
+        */
+        
+        //public static void int2bytes( int i, byte[] b, int offset )
+        SysLib.int2bytes(totalBlocks, tempSuperBlock, totalBlocksLoc);
+        SysLib.int2bytes(totalInodes, tempSuperBlock, totalInodeLoc);
+        SysLib.int2bytes(freeList, tempSuperBlock, freeListLoc);
+        //tempSuperBlock has the updated contents of the current file system's state.
+        SysLib.rawwrite(0, tempSuperBlock); //0th block is the super block.
+    }
+}
